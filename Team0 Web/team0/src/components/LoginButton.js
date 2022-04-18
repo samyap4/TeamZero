4b60aac67a74f9ccfbfcccfd89ad5c21a6ac64da
Sam Yapkowitz <samyapkowitz@fvfgr9g7q05n.attlocal.net>
2022-04-18 16:27:16 -0500

    First push

---
+++ Team0 Web/team0/src/components/LoginButton.js
@@ -0,0 +1,19 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import React from 'react'
+import '../App.css'
+
+function LoginButton(props) {
+    const { loginWithRedirect } = useAuth0();
+    return (
+        <div>
+            <button 
+                onClick={()=>loginWithRedirect({organization: props.organization, login_hint: props.email})} 
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+                Login
+            </button>
+        </div>
+    )
+}
+
+export default LoginButton
---
+++ Team0 Web/team0/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import './index.css';
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="auth.sam-yap.com"
+    clientId="T2b7ohcigKDCWn0P3ijNEkn1309Evl8q"
+    redirectUri={window.location.origin}
+  >
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
+);
